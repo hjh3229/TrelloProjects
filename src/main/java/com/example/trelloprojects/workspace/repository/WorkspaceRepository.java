e8290921175803760ff1e3dcd1a6a1fe6767a7ee
Eunbi Kim <eunbkim4@gmail.com>
2023-08-08 14:10:19 +0900

    [Fix] unused import 삭제

--- src/main/java/com/example/trelloprojects/workspace/repository/WorkspaceRepository.java
+++ src/main/java/com/example/trelloprojects/workspace/repository/WorkspaceRepository.java
@@ -1,11 +1,8 @@
 package com.example.trelloprojects.workspace.repository;
 
 import com.example.trelloprojects.workspace.entity.Workspace;
-import com.example.trelloprojects.workspace.enums.WorkspaceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
